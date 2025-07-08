94b9333355ef65a5501d223e1be0a4dfef43d9fc
sdramin <sdamin@gmail.com>
2025-07-08 01:57:49 -0300

    Delete app directory

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-BR">
-      <head />
-      <body>{children}</body>
-    </html>
-  );
-}
